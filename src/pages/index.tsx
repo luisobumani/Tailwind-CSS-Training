843596c9702cb2b4fa634d67365f74703d638889
Luis Obumani <l.obumani03@gmail.com>
2024-09-24 19:39:24 +0800

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,8 @@ export default function Home() {
     <div className="font-bold"> 
 
     <div className="h-screen bg-cover bg-center relative bg-[url('/images/awesomebackgroundcarsandhouses.jpg')]"/>
-    
+    <div className="w-screen h-screen bg-slate-900"></div>
+
   
     </div> 
   );
