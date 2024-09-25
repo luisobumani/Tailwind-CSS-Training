64438054f633848dd7491714f49d7f388c5aef70
Luis Obumani <l.obumani03@gmail.com>
2024-09-25 11:07:02 +0800

    updated css again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 export default function Home() {
   return (
-    //    header & background
-    <div className="font-bold"> 
+    <div className="relative h-screen font-bold">
 
-    <div className="h-screen bg-cover bg-center relative bg-[url('/images/awesomebackgroundcarsandhouses.jpg')]"/>
-    <div className="w-screen h-screen bg-slate-900"></div>
+      {/* Background Image */}
+      <div className="absolute inset-0 bg-cover bg-center bg-[url('/images/awesomebackgroundcarsandhouses.jpg')] z-10" />
 
-  
-    </div> 
+      {/* Background */}
+      <div className="absolute inset-0 bg-white z-20 opacity-60" />
+
+      {/* Header */}
+      <div className="relative z-30 flex items-center justify-center h-full">
+        <div className="flex flex-col items-start w-[90vw] max-w-[900px] h-[50vh] max-h-[500px] p-4">
+          <h1 className="text-blue-950 text-3xl sm:text-5xl font-sans mb-2">
+            Amaia Scapes Laguna
+          </h1>
+          <h2 className="text-green-900 text-2xl sm:text-4xl font-sans">
+            P R O P E R T Y  L I S T
+          </h2>
+        </div>
+      </div>
+
+      {/* Columns */}
+    </div>
   );
 }
